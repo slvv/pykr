cb7f792ac1bb34f8f9dc70c205b7fc8991669a89
Viacheslav Boesch <slavaboesch98@gmail.com>
2016-04-02 14:26:55 +0200

    created deck

--- src/pykr.py
+++ src/pykr.py
@@ -1,12 +1,36 @@
-class Suit: 
-    Hearts, Diamonds, Clubs, Spades = range(4)
-    
 
+suits = ["Hearts","Diamonds","Clubs","Spades"]
+ranks = ["Deuce","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace"]
+
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for suit in suits:
+            for rank in ranks:
+                card = Card(suit,rank)
+                self.cards.append(card)
+                print "Created card " + str(card)
+
+    def __str__(self):
+        return "deck with " + str(len(self.cards)) + " cards"
+
+
+
+class Card: 
+    def __init__(self, suit, rank):
+        self.suit = suit     
+        self.rank = rank
+    
+    def __str__(self):
+        return self.name()
+
+    def name(self):
+        return self.rank + " of " + self.suit
 
 def run():
-    print "test123" 
-
-
+    deck = Deck()
+    print deck
 
 
 if __name__ == "__main__":
